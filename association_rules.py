e6fe69295a3de8def77dc9c670a8eda1b5d7b708
AelaleA37 <alex.p.friedrichsen@gmail.com>
2023-03-07 16:25:15 -0500

    output for apriori and fp growth

--- association_rules.py
+++ association_rules.py
@@ -1,45 +1,75 @@
 import time
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import fpgrowth
 from mlxtend.frequent_patterns import apriori
+import pyECLAT
 
 h_dataset_files = ["T25I10D010K-h.txt", "T10I04D100K-h.txt"]
 v_dataset_files = ["T25I10D010K-v.txt", "T10I04D100K-v.txt"]
 
 # List the minimum support thresholds
 min_support_thresholds = [0.030, 0.025, 0.020, 0.015, 0.010, 0.005, 0.001]
+h_algorithms = ["fpgrowth", "apriori"]
+v_algorithms = ["eclat"]
 
-# Iterate over min_supports
-for min_support in min_support_thresholds:
-    #iterate over h_datasets
-    for i, h_file in enumerate(h_dataset_files):
-        output_file = "apriori_{}_{}".format(i, min_support)
-        with open(h_file, "r") as f:
-            transactions = [line.strip().split() for line in f]
+# for algorithm in h_algorithms:
+#     # Iterate over min_supports
+#     for min_support in min_support_thresholds:
+#         #iterate over h_datasets
+#         for i, h_file in enumerate(h_dataset_files):
+#             output_file = "output/{}_{}_{}".format(algorithm, i, min_support)
+#             with open(h_file, "r") as f:
+#                 transactions = [line.strip().split() for line in f]
 
-        te = TransactionEncoder()
-        te_ary = te.fit(transactions).transform(transactions)
-        df = pd.DataFrame(te_ary, columns=te.columns_)
+#             te = TransactionEncoder()
+#             te_ary = te.fit(transactions).transform(transactions)
+#             df = pd.DataFrame(te_ary, columns=te.columns_)
 
-        # Run the Apriori algorithm and time it
-        start_time = time.time()
-        frequent_itemsets = apriori(df, min_support=min_support, use_colnames=True)
-        execution_time = time.time() - start_time
+#             # Run algorithms and time them
+#             start_time = time.time()
+#             if algorithm == "apriori":
+#                 frequent_itemsets = apriori(df, min_support=min_support, use_colnames=True)
+#             elif algorithm == "fpgrowth":
+#                 frequent_itemsets = fpgrowth(df, min_support=min_support, use_colnames=True)
+#             execution_time = time.time() - start_time
 
-        # Write the frequent itemsets and execution time to the output file
-        with open(output_file, "w") as f:
-            f.write("Frequent itemsets:\n")
-            for index, row in frequent_itemsets.iterrows():
-                f.write(str(row['support']) + " : " + str(row['itemsets']) + "\n")
-            f.write("Execution time: " + str(execution_time) + " seconds")
+#             # Write the frequent itemsets and execution time to the output file
+#             with open(output_file, "w") as f:
+#                 f.write("Frequent itemsets:\n")
+#                 for index, row in frequent_itemsets.iterrows():
+#                     f.write(str(row['support']) + " : " + str(row['itemsets']) + "\n")
+#                 f.write("Execution time: " + str(execution_time) + " seconds")
 
-    # for v_file in v_dataset_files:
-    #     transactions = {}
-    #     for line in f:
-    #         item, transaction_id = line.strip().split()
-    #         if transaction_id not in transactions:
-    #             transactions[transaction_id] = set()
-    #         transactions[transaction_id].add(item)
-    #     transactions = [list(items) for items in transactions.values()]
+for algorithm in v_algorithms:
+    # Iterate over min_supports
+    for min_support in min_support_thresholds:
+        for i, dataset_file in enumerate(v_dataset_files):
+            # Read the transaction data from the file
+            with open(dataset_file, "r") as f:
+                transactions = [line.strip().split() for line in f]
+
+            # Convert the transaction data to a binary dataframe
+            df = pd.DataFrame(0, index=range(len(transactions)), columns=set.union(*[set(t) for t in transactions]))
+            for j, transaction in enumerate(transactions):
+                for item in transaction:
+                    df.at[j, item] = 1
+
+            # Initialize the Eclat object and time the computation
+            eclat = pyECLAT.ECLAT(data=df, verbose=False)
+            start_time = time.time()
+            eclat.fit(min_support=min_support, min_combination=1, max_combination=len(df.columns))
+            frequent_itemsets = eclat.items_
+            execution_time = time.time() - start_time
+
+            # Write the frequent itemsets and execution time to the output file
+            output_file = "output/{}_{}_{}.txt".format(algorithm, i, min_support)
+            with open(output_file, "w") as f:
+                f.write("Frequent itemsets:\n")
+                for itemset in frequent_itemsets:
+                    support = itemset[1] / len(transactions)
+                    itemset_str = " ".join(sorted(itemset[0].split(" & ")))
+                    f.write("{:.12f} : {}\n".format(support, itemset_str))
+                f.write("Execution time: {:.6f} seconds".format(execution_time))
 
 
---
+++ plot_results.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import re
+import os
+import numpy as np
+import pandas as pd
+
+# Define a function to extract min_support from the output filename
+def extract_min_support(filename):
+    pattern = r"_([0-9]+\.[0-9]+)$"
+    match = re.search(pattern, filename)
+    if match:
+        return float(match.group(1))
+    else:
+        return None
+
+def plot_execution_time(filename, title):
+    # Read the lines of the file
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    
+    # Extract the frequent itemsets and execution time
+    frequent_itemsets = []
+    execution_time = None
+    for line in lines:
+        if line.startswith("Frequent itemsets:"):
+            continue
+        elif line.startswith("Execution time:"):
+            execution_time = float(line.split(":")[1].replace("seconds", "").strip())
+        else:
+            parts = line.split(":")
+            support = float(parts[0].strip())
+            itemset = frozenset(parts[1].strip().split())
+            frequent_itemsets.append((itemset, support))
+    
+    # Extract the minimum support from the filename
+    min_support = extract_min_support(filename)
+    
+    # Plot the execution time vs minimum support
+    if min_support is not None and execution_time is not None:
+        plt.plot(min_support, execution_time, marker="o")
+        plt.xlabel("Minimum Support")
+        plt.ylabel("Execution Time (Seconds)")
+        plt.title("{} Execution Time vs Minimum Support".format(title))
+
+
+# Iterate over output files and plot execution times
+for filename in os.listdir("output"):
+    if filename.startswith("apriori"):
+        plot_execution_time(os.path.join("output", filename), "Apriori")
+    # elif filename.startswith("fp_growth"):
+    #     plot_execution_time(os.path.join("output", filename), "FP-Growth")
+    # elif filename.startswith("eclat"):
+    #     plot_execution_time(os.path.join("output", filename), "ECLAT")
+
+# Show the plot
+plt.show()
